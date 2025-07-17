5e6afbd4fbcc0ce2dae9fb7df8b79fb766f6aa35
DevOpsCodingArt <moiz@scholar.tu.edu.pk>
2025-07-17 16:10:42 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// src/index
-import { scan } from "react-scan"; // must be imported before React and React DOM
-import React from "react";
 
-scan({
-  enabled: true,
-});
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
